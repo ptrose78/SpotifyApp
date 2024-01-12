1864d51cc59a33b1e4de2173cb150fb1fc7cf8cb
Paul Rose <ptrose78@gmail.com>
2024-01-12 06:32:58 -0600

    Added styling and 'Save Playlist' functionality

--- src/Tracklist.js
+++ src/Tracklist.js
@@ -4,7 +4,8 @@ import Track from './Track';
 function Tracklist({tracks, onAddToPlaylist, isInPlaylist}) {
     
   return (
-    <div>
+    <div className='tracklist'>
+      <h3>Tracklist</h3>
       {tracks.map((track) => (
         <Track key={track.id} trackData={track} 
         onAddToPlaylist={onAddToPlaylist} isInPlaylist={isInPlaylist}/>
