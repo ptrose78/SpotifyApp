f2dc0fe95639def04bf604dad784f420df895f00
Paul Rose <ptrose78@gmail.com>
2024-01-12 07:23:16 -0600

    Get user's Spotify access token

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchResults from './SearchResults';
+import Spotify from './Spotify';
 
 function App() {
+  const [accessToken, setAccessToken] = useState('');
+
+  useEffect(() => {
+    // Call the method to get the access token
+    const token = Spotify.getAccessToken;
+
+    if (token) {
+      setAccessToken(token);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
